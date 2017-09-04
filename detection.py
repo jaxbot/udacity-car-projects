7c546ff0ec9b1234a9972a3c3ab1d5d73d4d3d4a
Jonathan Warner <jaxbot@gmail.com>
2017-09-04 00:31:47 -0400

    Add project

--- detection.py
+++ detection.py
@@ -9,45 +9,41 @@ from moviepy.editor import VideoFileClip
 import tensorflow as tf, sys
 from scipy.ndimage.measurements import label
 
-ystart = 300
-ystop = 656
-scale = 1.5
-spatial_size = (8, 8)
-hist_bins = 16
-
-heatmaps = []
-    
 classifier_definition = pickle.load(open("model.p", "rb"))
 
-svc = classifier_definition["classifier"]
-X_scaler = classifier_definition["x_scaler"]
-orient = classifier_definition["orientations"]
-pix_per_cell = classifier_definition["pixels_per_cell"]
-cell_per_block = classifier_definition["cell_per_block"]
+SVC = classifier_definition["classifier"]
+ORIENTATIONS = 11
+PIX_PER_CELL = 16
+CELL_PER_BLOCK = 2
+Y_START = 300
+Y_STOP = 656
+SCALE = 1.5
 
+OPTICAL_FLOW_COLORS = np.random.randint(0,255,(100,3))
+
+# Load TF image classifier
 label_lines = [line.rstrip() for line in tf.gfile.GFile("output_labels.txt")]
-
 with tf.gfile.FastGFile("output_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
+# Use a global session as starting one is expensive
 sess = tf.Session()
 
+# Classify a single ~299x299 image with CNN and return the probability of it being a vehicle
 def cnn_classify_subimage(subimg):
     global sess
     try:
-        start = time.time()
+        # TODO: Use the pixel data tensor to avoid having to jpeg encode features.
         image_data = cv2.imencode('.jpg', subimg)[1].tostring()
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
         predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
-        end = time.time()
-        print("Classifying window took ", end - start)
 
-        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        top_predictions = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
         results = {}
-        for node_id in top_k:
+        for node_id in top_predictions:
             human_string = label_lines[node_id]
             score = predictions[0][node_id]
 
@@ -58,53 +54,21 @@ def cnn_classify_subimage(subimg):
     except:
         return 0
 
-def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
-                        vis=False, feature_vec=True):
-    # Call with two outputs if vis==True
-    if vis == True:
-        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
-                                  visualise=vis, feature_vector=feature_vec)
-        return features, hog_image
-    # Otherwise call with one output
-    else:      
-        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
-                       visualise=vis, feature_vector=feature_vec)
-        return features
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, feature_vec=True):
+    features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                   cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
+                   visualise=False, feature_vector=feature_vec)
+    return features
 
-def convert_color(img, conv='RGB2YCrCb'):
-    if conv == 'RGB2YCrCb':
-        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    if conv == 'BGR2YCrCb':
-        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-    if conv == 'RGB2LUV':
-        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-
-def bin_spatial(img, size=(32, 32)):
-    color1 = cv2.resize(img[:,:,0], size).ravel()
-    color2 = cv2.resize(img[:,:,1], size).ravel()
-    color3 = cv2.resize(img[:,:,2], size).ravel()
-    return np.hstack((color1, color2, color3))
-                        
-def color_hist(img, nbins=32, bins_range=(0, 256)):
-    # Compute the histogram of the color channels separately
-    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
-    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
-    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
-    # Concatenate the histograms into a single feature vector
-    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
-    # Return the individual histograms, bin_centers and feature vector
-    return hist_features
-
-# Define a single function that can extract features using hog sub-sampling and make predictions
-def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
+# Extract features using hog sub-sampling and return potential bounding boxes
+# Taken from Udacity CarND Vehicle Tracking module
+def find_cars(img, ystart, ystop, scale, svc, orient, pix_per_cell, cell_per_block):
     boxes = []
     draw_img = np.copy(img)
-    img = img.astype(np.float32)/255
+    img = img.astype(np.float32) / 255
     
     img_tosearch = img[ystart:ystop,:,:]
-    ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_BGR2YUV) #convert_color(img_tosearch, conv='RGB2HLS')
+    ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_BGR2YUV)
     if scale != 1:
         imshape = ctrans_tosearch.shape
         ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
@@ -117,53 +81,39 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
     nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
     nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1 
     nfeat_per_block = orient*cell_per_block**2
-    
+
     # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
     window = 64
     nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
     cells_per_step = 2  # Instead of overlap, define how many cells to step
     nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
     nysteps = (nyblocks - nblocks_per_window) // cells_per_step
-    
+
     # Compute individual channel HOG features for the entire image
     hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
     hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
     hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    
+
     for xb in range(nxsteps):
         for yb in range(nysteps):
             ypos = yb*cells_per_step
             xpos = xb*cells_per_step
             # Extract HOG for this patch
-            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
             hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
 
             xleft = xpos*pix_per_cell
             ytop = ypos*pix_per_cell
 
-            # Extract the image patch
-            #subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
-
-            #test_prediction = cnn_classify_subimage(subimg)
-
-            # Get color features
-            #spatial_features = bin_spatial(subimg, size=spatial_size)
-            #hist_features = color_hist(subimg, nbins=hist_bins)
-
-            # Scale features and make a prediction
-            #test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))    
-            #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
-
             test_prediction = svc.predict(hog_features)
-            
+
             if test_prediction == 1:
                 xbox_left = np.int(xleft*scale)
                 ytop_draw = np.int(ytop*scale)
                 win_draw = np.int(window*scale)
                 boxes.append(((xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart)))
-                
     return boxes
 
 def add_heat(heatmap, bbox_list):
@@ -174,64 +124,52 @@ def add_heat(heatmap, bbox_list):
         heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
 
     # Return updated heatmap
-    return heatmap# Iterate through list of bboxes
-
-def apply_threshold(heatmap, threshold):
-    # Zero out pixels below the threshold
-    heatmap[heatmap <= threshold] = 0
-    # Return thresholded map
     return heatmap
 
-def draw_labeled_bboxes(img, vehicles):
+def draw_labeled_bboxes(img, bboxes):
+    # Iterate through all detected cars
+    for bbox in bboxes:
+        cv2.rectangle(img, bbox[0], bbox[1], COLORS[1], 3)
+    # Return the image
+    return img
+
+def draw_vehicles(img, vehicles):
     # Iterate through all detected cars
     for vehicle in vehicles:
         cv2.rectangle(img, vehicle.bbox[0], vehicle.bbox[1], vehicle.color, 3)
     # Return the image
     return img
 
-def average_heatmaps(heatmaps):
-    print(len(heatmaps))
-    if len(heatmaps) > 5:
-        heatmaps.pop(0)
-
-    average_heatmap = heatmaps[0]
-    if len(heatmaps) > 0:
-        for i in range(1, len(heatmaps)):
-            average_heatmap = np.logical_and(heatmaps[i], average_heatmap)
-            print("averaging")
-
-    return average_heatmap
-
-color = np.random.randint(0,255,(100,3))
+# Calculate the optical flow between two frames given previous tracked points
 def calculate_optical_flow(img, last_img, prev_pts):
-    global color
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     last_img = cv2.cvtColor(last_img, cv2.COLOR_BGR2GRAY)
 
     next_pts, status, err = cv2.calcOpticalFlowPyrLK(last_img, gray_img, prev_pts, None)
 
-    # Select good points
-    good_new = next_pts[status==1]
-    good_old = prev_pts[status==1]
+    good_new = next_pts[status == 1]
+    good_old = prev_pts[status == 1]
     flow_lines = (good_new,good_old)
 
     return next_pts, flow_lines
 
+# Draw optical flow lines on an image
 def draw_optical_flow_lines(img, flow_lines):
-    global color
+    global OPTICAL_FLOW_COLORS
     mask = np.zeros_like(img)
 
     i = 0
     for (new,old) in flow_lines:
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        frame = cv2.circle(img,(a,b),5,color[i].tolist(),-1)
+        x,y = new.ravel()
+        u,v = old.ravel()
+        mask = cv2.line(mask, (x,y),(u,v), OPTICAL_FLOW_COLORS[i].tolist(), 2)
+        frame = cv2.circle(img, (x, y), 5, OPTICAL_FLOW_COLORS[i].tolist(), -1)
         i = i + 1
-    img = cv2.add(img,mask)
+    img = cv2.add(img, mask)
 
     return img
 
+# Find flow lines and tracked points that exist within a bounding box
 def get_flow_within_box(flow_lines, bbox):
     retval = []
 
@@ -239,26 +177,28 @@ def get_flow_within_box(flow_lines, bbox):
         return retval
 
     for i,(new,old) in enumerate(zip(flow_lines[0], flow_lines[1])):
-        a,b = new.ravel()
-        c,d = old.ravel()
+        x,y = new.ravel()
 
-        if a >= bbox[0][0] and a <= bbox[1][0] and b >= bbox[0][1] and b <= bbox[1][1]:
+        if x >= bbox[0][0] and x <= bbox[1][0] and y >= bbox[0][1] and y <= bbox[1][1]:
             # Flow is within box
             retval.append((new, old))
 
     return retval
 
+# Calculate the movement between a set of flow lines
 def calculate_box_movement(flow_lines):
     avg_x = 0
     avg_y = 0
     count = 0
+
     for (new,old) in flow_lines:
-        a,b = new.ravel()
-        c,d = old.ravel()
+        x1,y1 = new.ravel()
+        x2,y2 = old.ravel()
 
-        x_mov = a - c
-        y_mov = b - d
+        x_mov = x1 - x2
+        y_mov = y1 - y2
 
+        # Ignore major shifts between frames caused by noisy tracking points
         if abs(x_mov) > 5 or abs(y_mov) > 5:
             print("Ignoring: X:", x_mov, "Y:", y_mov)
         else:
@@ -269,41 +209,45 @@ def calculate_box_movement(flow_lines):
         return (int((avg_x / count)), int((avg_y / count)))
     return (0, 0)
 
+# Returns whether the two bounding boxes collide
 def _bbox_collision(bbox1, bbox2):
-    #return bbox1[0][0] >= bbox2[0][0] and bbox1[0][0] <= bbox2[1][0] and bbox1[0][1] >= bbox2[0][1] and bbox1[0][1] <= bbox2[1][1]
     return bbox1[0][0] < bbox2[1][0] and bbox1[1][0] > bbox2[0][0] and bbox1[0][1] < bbox2[1][1] and bbox1[1][1] > bbox2[0][1]
 
+# Returns whether the two bounding boxes collide
 def bbox_collision(bbox1, bbox2):
     return _bbox_collision(bbox1, bbox2) or _bbox_collision(bbox2, bbox1)
 
+# Returns whether a vehicle's bounding box collides with existing tracked vehicles
 def vehicle_intersects(tracked_vehicles, vehicle):
     for existing_vehicle in tracked_vehicles:
         if bbox_collision(vehicle.bbox, existing_vehicle.bbox):
             return True
     return False
 
-def vehicle_is_subbox(tracked_vehicles, vehicle):
-    for existing_vehicle in tracked_vehicles:
-        bbox_e = existing_vehicle.bbox
-        bbox_v = vehicle.bbox
-        if bbox_v[0][0] > bbox_e[0][0] and bbox_v[1][0] < bbox_e[1][0] and bbox_v[0][1] > bbox_e[0][1] and bbox_v[1][1] < bbox_e[1][1]:
-            return True, existing_vehicle
-    return False, None
-
-
+# Finds new vehicles in an image
 def find_new_vehicles(img, tracked_vehicles, flow_lines):
-    boxes = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins)
+    # Find potential matches from HOG classifier
+    boxes = find_cars(img,
+            Y_START,
+            Y_STOP,
+            SCALE,
+            SVC,
+            ORIENTATIONS,
+            PIX_PER_CELL,
+            CELL_PER_BLOCK)
 
-
+    # Create a heatmap from HOG classifier
     heat = np.zeros_like(img[:,:,0]).astype(np.float)
     heat = add_heat(heat, boxes)
 
-    # Visualize the heatmap when displaying    
     heatmap = np.clip(heat, 0, 255)
     # Find final boxes from heatmap using label function
     labels = label(heatmap)
+
+    # Narrow down boxes using pixel CNN classifier
     final_boxes = cnn_classify(img, labels)
 
+    # Add any new vehicles that do not intersect with existing ones
     for box in final_boxes:
         vehicle = Vehicle()
         vehicle.bbox = box
@@ -313,34 +257,12 @@ def find_new_vehicles(img, tracked_vehicles, flow_lines):
         if not vehicle_intersects(tracked_vehicles, vehicle):
             tracked_vehicles.append(vehicle)
 
-    """
-    heat = np.zeros_like(img[:,:,0]).astype(np.float)
-    heat = add_heat(heat, final_boxes)
-    heatmap = np.clip(heat, 0, 255)
-    # Find final boxes from heatmap using label function
-    labels = label(heatmap)
-    for car_number in range(1, labels[1]+1):
-        # Find pixels with each car_number label value
-        nonzero = (labels[0] == car_number).nonzero()
-        # Identify x and y values of those pixels
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-        # Define a bounding box based on min/max x and y
-        box = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
-
-        vehicle = Vehicle()
-        vehicle.bbox = box
-        flow = get_flow_within_box(flow_lines, box)
-        vehicle.flow = flow
-
-        if not vehicle_intersects(tracked_vehicles, vehicle):
-            tracked_vehicles.append(vehicle)
-    """
-
+# Classify vehicles within a heatmap
 def cnn_classify(img, labels):
     bboxes = []
-
     boxes_to_test = []
+
+    # Create a list of bounding boxes from the input heatmap
     for car_number in range(1, labels[1]+1):
         # Find pixels with each car_number label value
         nonzero = (labels[0] == car_number).nonzero()
@@ -352,6 +274,7 @@ def cnn_classify(img, labels):
 
         boxes_to_test.append(bbox)
 
+    # Test each bounding box for vehicles
     for bbox in boxes_to_test:
         startx = bbox[0][0]
         endx = bbox[1][0]
@@ -361,26 +284,22 @@ def cnn_classify(img, labels):
         height = endy - starty
         width = endx - startx
 
+        # Ignore overly large or too small sections
         if width < 64 or height < 64 or width > 800 or height > 600:
             continue
 
-        padding = 1
-        if endx * padding <= img.shape[1]:
-            endx = int(endx * padding)
-        else:
-            endx = img.shape[1]
-
         img_section = img[starty:endy, startx:endx]
+        # Run a classification on the entire bounding box, and discard if there's a low chance of
+        # vehicles being found within it.
         overall_results = cnn_classify_subimage(img_section)
         if overall_results < 0.1:
             print("Overall results:", overall_results)
             continue
-        #cv2.imshow('image',img_section)
-        #cv2.waitKey(0)
 
         width = img_section.shape[1]
         height = img_section.shape[0]
 
+        # Create an image with 299 as the minimal dimension
         if width > height:
             width = width / height * 299
             height = 299
@@ -394,10 +313,13 @@ def cnn_classify(img, labels):
         vehicles = []
         x_shifts = []
 
+        # Generate a list of window shifts to iterate through (just makes the math easier).
         for x_shift in range(0, int(width - 299), 64):
             x_shifts.append(x_shift)
         x_shifts.append(int(width - 299))
 
+        # Sweep the image horizontally looking for vehicles in 299x299 squares
+        # This helps find cases where two vehicles are side by side and included in a single heatmap label
         for x_shift in x_shifts:
             img_window = img_section[0:299, x_shift:299 + x_shift]
 
@@ -406,6 +328,7 @@ def cnn_classify(img, labels):
             offset = (endx - startx) / width
             bbox = ((startx + int(x_shift * offset), starty), (startx + int((x_shift + 299) * offset), bbox[1][1]))
             found = False
+            # If this bounding box collides with others, only accept it if its probability is higher
             for vehicle in vehicles:
                 if bbox_collision(vehicle.bbox, bbox):
                     if vehicle.probability < results:
@@ -413,6 +336,7 @@ def cnn_classify(img, labels):
                         vehicle.probability = results
                         found = True
                         break
+            # If it does not collide and it meets the minimum threshold, accept it
             if found == False and results > 0.8:
                 vehicle = Vehicle()
                 vehicle.bbox = bbox
@@ -421,10 +345,9 @@ def cnn_classify(img, labels):
         for vehicle in vehicles:
             bboxes.append(vehicle.bbox)
 
-    print("Roudn complete!")
-    print(bboxes)
     return bboxes
 
+# Bounding box colors
 COLORS = [(66, 134, 244),
         (66, 80, 244),
         (200, 66, 244),
@@ -433,9 +356,9 @@ COLORS = [(66, 134, 244),
         (244, 155, 66),
         (173, 244, 66),
         (66, 244, 167)]
-
 current_color = 0
 
+# Class of a tracked vehicle bounding box.
 class Vehicle:
     bbox = []
     flow = []
@@ -449,4 +372,3 @@ class Vehicle:
         current_color += 1
         if current_color >= len(COLORS):
             current_color = 0
-
--- project.py
+++ project.py
@@ -15,18 +15,18 @@ from imutils import contours
 from skimage import measure
 import time
 
-
-
+# Global state shared between frames
 last_img = None
 prev_pts = None
-
 tracked_vehicles = []
-
 framecount = 0
 debounce = 0
 
+# Process a single frame based on state from previous iterations, if available.
 def process_frame(img):
-    global heatmaps, last_img, prev_pts, tracked_vehicles, framecount, debounce
+    global last_img, prev_pts, tracked_vehicles, framecount, debounce
+
+    # Create optical flow points if none exist
     if last_img is None:
         last_img = img
         last_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -38,9 +38,8 @@ def process_frame(img):
 
     framecount += 1
     debounce += 1
-    #tracked_vehicles = []
-    #find_new_vehicles(img, tracked_vehicles, flow_lines)
 
+    # If no vehicles have been found and we haven't checked in 5 frames, or every 15 frames regardless, look for vehicles.
     if (len(tracked_vehicles) < 1 and debounce > 5) or framecount > 15:
         last_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         prev_pts = cv2.goodFeaturesToTrack(last_gray, maxCorners = 100, qualityLevel = 0.3, minDistance = 7)
@@ -48,9 +47,11 @@ def process_frame(img):
         framecount = 0
         debounce = 0
 
+    # Iterate through every vehicle we are currently tracking, if any
     i = 0
     tracked_vehicles_copy = tracked_vehicles[:]
     for vehicle in tracked_vehicles_copy:
+        # Move the bounding box based on optical flow within its bounding box
         movement = calculate_box_movement(vehicle.flow)
         new_box = ((vehicle.bbox[0][0] + movement[0], vehicle.bbox[0][1] + movement[1]),
                 (vehicle.bbox[1][0] + movement[0], vehicle.bbox[1][1] + movement[1]))
@@ -61,9 +62,12 @@ def process_frame(img):
         vehicle_subimage = img[new_box[0][1]:new_box[1][1], new_box[0][0]:new_box[1][0]]
         results = cnn_classify_subimage(vehicle_subimage)
         vehicle.probability = results
+
         print("The vehicle we expect to be has score ", results)
+
         if results < 0.8:
             tracked_vehicles[i].missing_frames += 1
+            # If the vehicle does not exist for 5 frames, remove it and check for new vehicles next frame
             if tracked_vehicles[i].missing_frames > 4:
                 tracked_vehicles.remove(vehicle)
                 debounce = 5
@@ -73,27 +77,23 @@ def process_frame(img):
             tracked_vehicles[i].missing_frames = 0
         i += 1
 
+    # Draw tracked vehicles that still exist
     for vehicle in tracked_vehicles:
         flow = get_flow_within_box(flow_lines, vehicle.bbox)
         vehicle.flow = flow
         draw_text(img, str(vehicle.probability), (vehicle.bbox[0][0], vehicle.bbox[0][1]))
         img = draw_optical_flow_lines(img, flow)
 
-    draw_img = draw_labeled_bboxes(np.copy(img), tracked_vehicles)
-
-
-    #cv2.imshow('image',draw_img)
-    #cv2.waitKey(0)
-
-
+    draw_img = draw_vehicles(np.copy(img), tracked_vehicles)
 
     return draw_img
 
-
+# Draws text on an imagine at a specified location
 def draw_text(img, text, pos):
     cv2.putText(img, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-output_file = 'output_dl_orlando3.mp4'
-clip = VideoFileClip("00280001.mp4")#.subclip(20, 240)
+# Load video and kick off pipeline
+output_file = 'tmp.mp4'
+clip = VideoFileClip("project_video.mp4").subclip(28, 30)
 processed_clip = clip.fl_image(process_frame)
 processed_clip.write_videofile(output_file, audio=False)
