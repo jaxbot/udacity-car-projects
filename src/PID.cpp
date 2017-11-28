7142c0200ef2b4e25a4c47ef9f077168113ccf76
Jonathan Warner <jaxbot@gmail.com>
2017-11-27 22:22:04 -0500

    PID algorithm

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,27 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+    if (p_error == 0) {
+        p_error = cte;
+    }
+
+    d_error = cte - p_error;
+    p_error = cte;
+
+    i_error += cte;
 }
 
 double PID::TotalError() {
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
